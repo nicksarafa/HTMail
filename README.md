37461701573179301270e3fcc557b7406c866a2a
nsarafa <nicksarafa@gmail.com>
2016-04-03 22:14:29 -0400

    update with latest gulpfile and components

--- README.md
+++ README.md
@@ -1,22 +1,49 @@
-# HTMail Builder 
+# HTMail Builder
 
-Expedite HTML email development with the help of Gulp, SASS, HAML, Inline CSS
+Expedite marketing and transactional HTML email development with the help of Gulp, SASS, HAML, Inline CSS and (Litmus)[https://litmus.com/checklist] for cross-app and cross-browser testing
 
-### Setup 
+### Example Emails
+* [Action email]()
+* [Email alert]()
+* [Billing email]()
 
-Clone the repo
+### What are transactional emails? (source)[https://raw.githubusercontent.com/mailgun/transactional-email-templates/master/README.md]
+
+Typically any email that is triggered by or sent automatically from your application.
+
+* Welcome emails
+* Actionable emails
+* Password resets
+* Receipts
+* Monthly invoices
+* Support requests
+* App error alerts
+* Reminders
+* etc.
+
+### Setup
+
+* Make sure you have at least Node 4.3 installed by running
+
+`node --version`
+
+* Clone this repo
 
 `git clone https://github.com/nsarafa/HTMail.git`
+
+* Change directory into it
+
+`cd your/path/to/HTMail`
 
 Install all the things
 
 `npm install`
 
-### How to..
+### Using Gulp
 
 Compile HAML to HTML
 
-`gulp sass`
+`gulp haml`
 
 Compile SASS to CSS
 
@@ -29,3 +56,36 @@ Inline your CSS, as well as compile your SASS to CSS
 Compile your HAML, SCSS while Inlining the compiled CSS into your HTML in real time
 
 `gulp watch`
+
+### Running Tests
+
+1. After you have compiled your email, run `gulp test`
+2. Open (Litmus email dashboard)[https://litmus.com/checklist]
+3. Click on your email
+ * If you don't have Litmus account please see __Litmus Test Setup__ below and set up your account
+
+#### Litmus HTML Email Testing Suite Setup
+ 
+1. Open `gulpfile.js`
+1. Find the Litmus configuration and update the configuration with your litmus username, password, organization's litmus URL
+2. Next, choose email clients to test email against by..
+ * List of available email test clients can be found here ~> https://YOURAPPLICATIONNAME.litmus.com/emails/clients.xml
+ * After you pick the clients you want to test, copy their <application_code>, open gulpfile.js and add your chose <application_code> to the litmus config applications array 
+3. Save gulpfile.js and run `gulp test`
+4. See your email test results here ~> https://litmus.com/checklist/
+
+#### TODO
+
+* Explore alternatives to Litmus for cross-app and cross-browser email testing
+* Re-write `gulp compile` function with gulp-css-inline and test deployment included
+* Re-introduce gulp-css-inline
+* Integrate documentation markdown tool for inline documentation that outputs to wiki
+* Expand module base
+* Introduce linters for code consistency
+* Explain src/ -> build/ process
+* Integrate (BrowserSync)[https://www.browsersync.io/] 
+* Consolidate gulp functions
+* Introduce Outlook specific styles (Reference)[http://templates.mailchimp.com/development/css/outlook-conditional-css/]
+* Introduce Style reset (Reference)[http://templates.mailchimp.com/development/css/reset-styles/] 
+* Add example email templates
+* Update href tags inside of footer with correct links
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,100 @@
-// Initialize Gulp
+// =============================================================================
+// GULPFILE.JS / Custom gulp functions 
+// =============================================================================
+
+// Initialize gulp 
+// -----------------------------------------------------------------------------
+
 var gulp = require('gulp'),
     haml = require('gulp-ruby-haml'),
+    inlineCss = require('gulp-inline-css'),
+    litmus = require('gulp-litmus');
     sass = require('gulp-sass');
 
-// Compile SCSS to CSS
+// Compile src/*.scss to build/*.css
+// -----------------------------------------------------------------------------
+
 gulp.task('sass', function() {
-  return gulp.src('app/sass/*.scss')
+  return gulp.src('src/sass/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('build/css/.'));
 });
+ 
+// Compile src/*.haml to build/*.html
+// -----------------------------------------------------------------------------
 
-// Compile HAML to HTML
 gulp.task('haml', function() {
-  return gulp.src('app/haml/*.haml')
+  return gulp.src(['src/haml/*.haml', 'src/haml/modules/*.haml', 'src/haml/templates/marketing/*/*.haml', 'src/haml/templates/transactional/*.haml'])
     .pipe(haml())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('build/.'));
 });
 
-// Inline compiled CSS to dist
+// Compile src/*.scss to build/*.css
+// -----------------------------------------------------------------------------
+
 gulp.task('inline', ['sass', 'haml'], function() {
-  return gulp.src(['app/./*.html', './dist/*.html'])
-    .pipe(gulp.dest('./dist'));
+  return gulp.src(['build/*.html'])
+		.pipe(inlineCss())
+    .pipe(gulp.dest('build/.'));
 });
 
-// Compile SASS and html
-gulp.task('dist', ['inline']);
+// Compile sass, and haml, inline styles and dispatch new test to Litmus 
+// -----------------------------------------------------------------------------
 
-// Watch all the things
+gulp.task('build', ['inline', 'sass', 'haml', 'test'], function() {
+  return gulp.src(['build/*.html'])
+    .pipe(gulp.dest('build/.'));
+});
+
+// Automatically compile stylesheet(s) and haml while inlining 
+// -----------------------------------------------------------------------------
+
 gulp.task('watch', function() {
-    gulp.watch('app/sass/*.scss', ['sass']);
-    gulp.watch('app/haml/*.h*ml', ['inline']);
+    gulp.watch('src/sass/*.scss', ['build']);
+    gulp.watch(['src/haml/modules/*.haml', 'src/haml/templates/transactional/*.haml', 'src/haml/templates/marketing/**/*.haml'], ['build']);
+});
+
+// Configure Litmus email testing tool 
+// -----------------------------------------------------------------------------
+//
+// Include Your Litmus Username, Password, Our Organization's URL and List Of Emails You Want To Test Against
+// List of Available Testing Clients Can Be Found Here ~> https://YOURAPPLICATIONNAME.litmus.com/emails/clients.xml
+// Pick The Clients You Want To Test Against, Copy Their <application_code> And Add It To The Array Of applications: [] below
+// 
+// REFERENCES
+// -----------------------------------------------------------------------------
+//
+// * https://litmus.com
+//
+// -----------------------------------------------------------------------------
+ 
+var config = {
+    username: 'YOURLITMUSUSERNAME',
+    password: 'YOURLITMUSPASSWORD',
+    url: 'https://YOURAPPLICATIONNAME.litmus.com',
+    applications: [
+        'appmail8',
+        'gmailnew',
+        'ffgmailnew',
+        'chromegmailnew',
+        'iphone5s',
+    ]
+}
+
+// Test emails across different applications, browsers and devices 
+// -----------------------------------------------------------------------------
+// 
+// See test using name and password inside 'Litmus Email Testing Config'
+//
+// REFERENCES
+// -----------------------------------------------------------------------------
+//
+// * https://litmus.com/checklist/
+//
+// -----------------------------------------------------------------------------
+
+gulp.task('test', function () {
+    return gulp.src('build/index.html')
+        .pipe(litmus(config))
+        .pipe(gulp.dest('build/.'));
 });
